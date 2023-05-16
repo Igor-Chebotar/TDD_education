aac5aa66a17e9d86c30b7161d9265e13d0f6d643
Igor_Chebotar <igorcebotar5@gmail.com>
2023-05-17 00:37:38 +0300

    add decorator csrf_protect to lists/views.py

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,22 +1,22 @@
 from django.shortcuts import render, redirect
 from lists.models import Item, List
-from django.views.decorators.csrf import requires_csrf_token, csrf_exempt
+from django.views.decorators.csrf import requires_csrf_token, csrf_exempt, csrf_protect
 
 
-@csrf_exempt
+@csrf_protect
 def home_page(request):
     '''домашная страница'''
     return render(request, 'home.html')
 
 
-@csrf_exempt
+@csrf_protect
 def view_list(request, list_id):
     '''представление списка'''
     list_ = List.objects.get(id=list_id)
     return render(request, 'list.html', {'list': list_})
 
 
-@csrf_exempt
+@csrf_protect
 def new_list(request):
     '''новый список'''
     list_ = List.objects.create()
@@ -24,7 +24,7 @@ def new_list(request):
     return redirect(f'/lists/{list_.id}')
 
 
-@csrf_exempt
+@csrf_protect
 def add_item(request, list_id):
     list_ = List.objects.get(id=list_id)
     Item.objects.create(text=request.POST['item_text'], list=list_)
