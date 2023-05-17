cdfa5244a70884b41f0ee014b7bec012d2eaf0ca
Igor_Chebotar <igorcebotar5@gmail.com>
2023-05-17 23:08:40 +0300

    @csrf_exempt

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,22 +1,22 @@
 from django.shortcuts import render, redirect
 from lists.models import Item, List
-from django.views.decorators.csrf import requires_csrf_token
+from django.views.decorators.csrf import csrf_exempt
 
 
-@requires_csrf_token
+@csrf_exempt
 def home_page(request):
     '''домашная страница'''
     return render(request, 'home.html')
 
 
-@requires_csrf_token
+@csrf_exempt
 def view_list(request, list_id):
     '''представление списка'''
     list_ = List.objects.get(id=list_id)
     return render(request, 'list.html', {'list': list_})
 
 
-@requires_csrf_token
+@csrf_exempt
 def new_list(request):
     '''новый список'''
     list_ = List.objects.create()
@@ -24,7 +24,7 @@ def new_list(request):
     return redirect(f'/lists/{list_.id}')
 
 
-@requires_csrf_token
+@csrf_exempt
 def add_item(request, list_id):
     list_ = List.objects.get(id=list_id)
     Item.objects.create(text=request.POST['item_text'], list=list_)
