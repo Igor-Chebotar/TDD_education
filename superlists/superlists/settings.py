f2c9789b56001fee84b797bf72ac4036122773ae
Igor_Chebotar <igorcebotar5@gmail.com>
2023-05-18 16:36:38 +0300

    add CSRF_TRUSTED_ORIGINS in settings.py

--- superlists/superlists/settings.py
+++ superlists/superlists/settings.py
@@ -117,8 +117,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 # STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
-STATICFILES_DIRS = [ os.path.join(BASE_DIR, '../static'), ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+if DEBUG:
+    CSRF_TRUSTED_ORIGINS = ['http://*', 'https://*']
+# if not DEBUG:
+#     CSRF_TRUSTED_ORIGINS = ['http://*.tdd-test-project.ru', 'https://*.tdd-test-project.ru']
