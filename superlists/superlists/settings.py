cc0aec1fca5d327912c5ab81ef8407e0d7415085
Igor_Chebotar <igorcebotar5@gmail.com>
2023-05-18 17:37:56 +0300

    update ALLOWED_HOSTS in settings.py

--- superlists/superlists/settings.py
+++ superlists/superlists/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-_8w(zgz@gvjwa@s#xsli3tay9p90rce*5++%557)mp^^vclad4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 # Application definition
 
