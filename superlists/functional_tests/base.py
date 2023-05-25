45ac2ed8b306852ae700233ffed56db16481c28d
Igor_Chebotar <igorcebotar5@gmail.com>
2023-05-26 02:50:57 +0300

    Перемещение ФТ в свои собственные отдельные файлы

---
+++ superlists/functional_tests/base.py
@@ -0,0 +1,42 @@
+import time
+import os
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.common.by import By
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium.common.exceptions import WebDriverException
+from selenium.webdriver.firefox.options import Options
+
+MAX_WAIT = 10
+
+
+class FunctionalTest(StaticLiveServerTestCase):
+    '''функциональный тест'''
+
+    def setUp(self):
+        '''установка'''
+        options = Options()
+        options.headless = True
+        self.browser = webdriver.Firefox(options=options, service=FirefoxService(GeckoDriverManager().install()), )
+        staging_server = os.environ.get('STAGING_SERVER')
+        if staging_server:
+            self.live_server_url = 'http://' + staging_server
+
+    def tearDown(self):
+        '''демонтаж'''
+        self.browser.quit()
+
+    def wait_for_row_in_list_table(self, row_text):
+        '''ожидать строку в таблице списка'''
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element(By.ID, 'id_list_table')
+                rows = table.find_elements(By.TAG_NAME, 'tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
--- superlists/functional_tests/test_simple_list_creation.py
+++ superlists/functional_tests/test_simple_list_creation.py
@@ -1,47 +1,14 @@
 import time
-import os
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-from selenium.common.exceptions import WebDriverException
-from selenium.webdriver.firefox.options import Options
-
-MAX_WAIT = 10
+from .base import FunctionalTest
 
 
-class NewVisitorTest(StaticLiveServerTestCase):
+class NewVisitorTest(FunctionalTest):
     '''тест нового посетителя'''
-
-    def setUp(self):
-        '''установка'''
-        options = Options()
-        options.headless = True
-        self.browser = webdriver.Firefox(options=options, service=FirefoxService(GeckoDriverManager().install()), )
-        staging_server = os.environ.get('STAGING_SERVER')
-        if staging_server:
-            self.live_server_url = 'http://' + staging_server
-
-
-    def tearDown(self):
-        '''демонтаж'''
-        self.browser.quit()
-
-    def wait_for_row_in_list_table(self, row_text):
-        '''ожидать строку в таблице списка'''
-        start_time = time.time()
-        while True:
-            try:
-                table = self.browser.find_element(By.ID, 'id_list_table')
-                rows = table.find_elements(By.TAG_NAME, 'tr')
-                self.assertIn(row_text, [row.text for row in rows])
-                return
-            except (AssertionError, WebDriverException) as e:
-                if time.time() - start_time > MAX_WAIT:
-                    raise e
-                time.sleep(0.5)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         '''тест: можно начать список и получить его позже'''
@@ -130,30 +97,3 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.assertIn('Купить молоко', page_text)
 
         # Удовлетворенные, они оба ложатся спать
-
-    def test_layout_and_styling(self):
-        '''тест макета и стилевого оформления'''
-        # Эдит открывает домашнюю страницу
-        self.browser.get(self.live_server_url)
-        self.browser.set_window_size(1024, 768)
-
-        # Она замечает, что поле ввода аккуратно центрировано
-        inputbox = self.browser.find_element(By.ID, 'id_new_item')
-        self.assertAlmostEqual(
-            inputbox.location['x'] + inputbox.size['width'] / 2,
-            512,
-            delta=10
-        )
-
-        # Она начинает новый список и видит, что поле ввода там тоже
-        # аккуратно центировано
-        inputbox.send_keys('testing')
-        inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: testing')
-        inputbox = self.browser.find_element(By.ID, 'id_new_item')
-        self.assertAlmostEqual(
-            inputbox.location['x'] + inputbox.size['width'] / 2,
-            512,
-            delta=10
-        )
-
