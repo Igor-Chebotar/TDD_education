a767dfb2b29cb39521ffbdd5c04bafe0d72e8b65
Igor_Chebotar <igorcebotar5@gmail.com>
2023-05-31 00:36:57 +0300

    Реализована валидация наличия повторяющегося элемента на уровне формы

--- superlists/lists/tests/test_views.py
+++ superlists/lists/tests/test_views.py
@@ -1,8 +1,8 @@
 from django.test import TestCase
 from lists.models import Item, List
 from django.utils.html import escape
-from lists.forms import ItemForm, EMPTY_ITEM_ERROR
-
+from lists.forms import DUPLICATE_ITEM_ERROR, EMPTY_ITEM_ERROR, ExistingListItemForm, ItemForm
+from unittest import skip
 
 class HomePageTest(TestCase):
     '''тест домашней страницы'''
@@ -100,12 +100,27 @@ class ListViewTest(TestCase):
     def test_for_invalid_input_passes_form_to_template(self):
         '''тест на недопустимый ввод: форма передается в шаблон'''
         response = self.post_invalid_input()
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
 
     def test_for_invalid_input_shows_error_on_page(self):
         '''тест на недопустимый ввод: на странице показывается ошибка'''
         response = self.post_invalid_input()
         self.assertContains(response, escape(EMPTY_ITEM_ERROR))
+
+
+    def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
+        '''тест: ошибки валидации повторяющегося элемента
+        оканчиваются на странице списков'''
+        list1 = List.objects.create()
+        item1 = Item.objects.create(list=list1, text='textey')
+        response = self.client.post(
+            f'/lists/{list1.id}',
+            data={'text': 'textey'}
+        )
+        expected_error = escape(DUPLICATE_ITEM_ERROR)
+        self.assertContains(response, expected_error)
+        self.assertTemplateUsed(response, 'list.html')
+        self.assertEqual(Item.objects.all().count(), 1)
 
     def test_invalid_list_items_arent_saved(self):
         '''тест: не сохраняются недопустимые элементы списка'''
@@ -133,5 +148,5 @@ class ListViewTest(TestCase):
         '''тест отображения формы для элемента'''
         list_ = List.objects.create()
         response = self.client.get(f'/lists/{list_.id}')
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
         self.assertContains(response, 'name="text"')
